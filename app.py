6de3af4747ec520ea84a664ae57491e58765ae47
Timothy Madegwa <timothymadegwa@gmail.com>
2023-02-23 11:18:07 +0300

    contact form

--- app.py
+++ app.py
@@ -34,12 +34,5 @@ def send_mail():
 
     return render_template('index.html')
 
-'''
-@app.route('/predict',methods=['POST'])
-def predict():    
-
-    return render_template('report.html', text='Client {} : Probability of repayment for {} {} {} on a loan of KES {} is: {} %'.format(customer_id, title, f_name, l_name, amt, output))
-    
-'''
 if __name__ == "__main__":
     app.run(debug=False)
