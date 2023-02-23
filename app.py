4f39d867d6f200a2be42ffab0c2c4cf7fb3513f8
Timothy Madegwa <timothymadegwa@gmail.com>
2023-02-23 10:57:22 +0300

    added email contact form

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 from flask import Flask, request, jsonify, render_template
+from flask_mail import Mail, Message
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 
 app = Flask(__name__)
+email_sender = os.getenv("EMAIL_HOST_USER")
+app.config["MAIL_SERVER"] = os.getenv("EMAIL_HOST")
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = email_sender
+app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_HOST_PASSWORD")
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 
+mail = Mail(app)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        msg = Message(subject="Hello", sender=email_sender, recipients=["timothymadegwa@gmail.com"])
+        msg.body = "Test Email"
+        mail.send(msg)
+        return "sent"
+
     return render_template('index.html')
 
+@app.route('/send_mail')
+def send_mail():
+    msg = Message(subject="Hello", sender=email_sender, recipients=["timothymadegwa@gmail.com"])
+    msg.body = "Test Email"
+    mail.send(msg)
+
+    return render_template('index.html')
 
 '''
 @app.route('/predict',methods=['POST'])
